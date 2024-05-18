46cd2ed5db900af046558a8504a5787cf94f9ede
sunseo18 <sunseo1818@gmail.com>
2024-05-19 00:19:40 +0900

    [ADD] Repository 추가

--- src/main/java/com/example/sopkathon/domain/Stone.java
+++ src/main/java/com/example/sopkathon/domain/Stone.java
@@ -16,7 +16,7 @@ import lombok.NoArgsConstructor;
 public class Stone {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     private String uglyImage;
 
---
+++ src/main/java/com/example/sopkathon/domain/StoneJpaRepository.java
@@ -0,0 +1,6 @@
+package com.example.sopkathon.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoneJpaRepository extends JpaRepository<Stone, Long> {
+}
