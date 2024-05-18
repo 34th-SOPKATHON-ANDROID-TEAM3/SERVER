d7c7e52daa296b51a7b2e6b2bd93e0ff08338184
sunseo18 <sunseo1818@gmail.com>
2024-05-19 01:25:21 +0900

    [FEAT] 오늘의 돌 조회하기 api 구현 #1

--- src/main/java/com/example/sopkathon/domain/StoneJpaRepository.java
+++ src/main/java/com/example/sopkathon/domain/StoneJpaRepository.java
@@ -3,4 +3,5 @@ package com.example.sopkathon.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StoneJpaRepository extends JpaRepository<Stone, Long> {
+    Stone findFirstByOrderByIdDesc();
 }
