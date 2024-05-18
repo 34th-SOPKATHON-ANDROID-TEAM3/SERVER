7b95d0f38b15a8c8d4ae28d4c8e460db752bd90b
sunseo18 <sunseo1818@gmail.com>
2024-05-19 03:27:33 +0900

    [REFACTOR] 컨트롤러, 서비스 객체명 변경 #9

--- src/main/java/com/example/sopkathon/stone/controller/StoneController.java
+++ src/main/java/com/example/sopkathon/stone/controller/StoneController.java
@@ -1,6 +1,6 @@
 package com.example.sopkathon.stone.controller;
 
-import com.example.sopkathon.stone.JunseoStoneService;
+import com.example.sopkathon.stone.StoneService;
 import com.example.sopkathon.stone.controller.dto.GetSimpleStoneResponseDto;
 import com.example.sopkathon.stone.controller.dto.GetStoneOfTodayResponseDto;
 import com.example.sopkathon.stone.controller.dto.GetStoneResponseDto;
@@ -18,32 +18,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/stones")
 @RestController
-public class JunseoStoneController {
+public class StoneController {
 
-    private final JunseoStoneService junseoStoneService;
+    private final StoneService stoneService;
 
     @GetMapping("/today")
     public ResponseEntity<GetStoneOfTodayResponseDto> getStoneOfToday() {
-        return ResponseEntity.ok(junseoStoneService.getStoneOfToday());
+        return ResponseEntity.ok(stoneService.getStoneOfToday());
     }
 
     @PatchMapping("/today")
     public ResponseEntity<Void> updateAnswerStoneOfToday(
             @RequestBody UpdateAnswerOfStoneRequestDto request
     ) {
-        junseoStoneService.updateAnswerOfStone(request);
+        stoneService.updateAnswerOfStone(request);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping
     public ResponseEntity<List<GetSimpleStoneResponseDto>> getAllSimpleStone() {
-        return ResponseEntity.ok(junseoStoneService.getAllSimpleStone());
+        return ResponseEntity.ok(stoneService.getAllSimpleStone());
     }
 
     @GetMapping("/{stoneId}")
     public ResponseEntity<GetStoneResponseDto> getStone(
             @PathVariable Long stoneId
     ) {
-        return ResponseEntity.ok(junseoStoneService.getStone(stoneId));
+        return ResponseEntity.ok(stoneService.getStone(stoneId));
     }
 }
